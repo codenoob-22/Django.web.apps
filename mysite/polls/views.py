eaeb8d6be3880f8301845cb16d411140f8df2073
Sushil Sarwade <sushil.work100@gmail.com>
2020-01-05 03:21:11 +0530

    got 404 changes and added template folder

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, Http404
+from .models import Question, Choice
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, World")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    for_frontend = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', for_frontend)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', { 'question': question })
+
+def results(request, question_id):
+    return HttpResponse(" nothing")
+
+def vote(request, question_id):
+    return HttpResponse(" nothing")
