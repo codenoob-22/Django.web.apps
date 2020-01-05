223f41a8f8654590da2784c36cbf312c73a02be4
Sushil Sarwade <sushil.work100@gmail.com>
2020-01-05 11:56:47 +0530

    sdosaidj

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path( '', views.index, name='index'),
-    path( '<int:question_id>/', views.detail, name='index'),
-    path( '<int:question_id>/results/', views.results, name='index'),
-    path( '<int:question_id>/vote/', views.vote, name='index'),
+    path( '<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path( '<int:pk>/results/', views.Resultsview.as_view(), name='results'),
+    path( '<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,21 +1,26 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.urls import reverse
+from django.views import generic
 from .models import Question, Choice
+
 # Create your views here.
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    for_frontend = {
-        'latest_question_list': latest_question_list,
-    }
-    return render(request, 'polls/index.html', for_frontend)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions"""
+        return Question.objects.order_by('-pub_date')[:5]
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', { 'question': question })
 
 def results(request, question_id):
-    return HttpResponse(" nothing")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html',{'question':question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
