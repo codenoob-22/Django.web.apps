cebc2c6267c16fbe9ec3144cb74b2e89df62bdf2
Sushil Sarwade <sushil.work100@gmail.com>
2020-01-05 10:29:21 +0530

    change vote definition

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from .models import Question, Choice
 # Create your views here.
 
@@ -18,4 +18,18 @@ def results(request, question_id):
     return HttpResponse(" nothing")
 
 def vote(request, question_id):
-    return HttpResponse(" nothing")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        #redisplay the question voting form
+        return render(request, 'polls/detail.html', {
+            'question':question,
+            'error_message':"you didn't select any choice",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        #always return an HttpResponseRedirect after succesfully dealing with POST data.
+        #this prevents data from being posted twice if user hits the back button
+        return HttpResponseRedirect(reverse('polls:results',args=(question_id, )))
